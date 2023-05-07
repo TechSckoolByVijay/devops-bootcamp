38bdc117df4a511b9fb1ccca56f020cca2f55b38
Saini Vijay <vijaysainiprofessional@gmail.com>
2023-05-07 19:05:11 +0530

    ============VERSION1========

--- ApplicationCode/Services/Django-app/shopping_cart/api_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CartItemViews
-
-urlpatterns = [
-    path('cart-items', CartItemViews.as_view()),
-    path('cart-items/<int:id>', CartItemViews.as_view())
-]
