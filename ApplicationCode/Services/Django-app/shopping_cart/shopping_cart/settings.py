fd7c5dfbac5b41e2b6559f05a960b18e7fb428ab
Saini Vijay <vijaysainiprofessional@gmail.com>
2023-05-11 22:39:10 +0530

    wdw

--- ApplicationCode/Services/Django-app/shopping_cart/shopping_cart/settings.py
+++ ApplicationCode/Services/Django-app/shopping_cart/shopping_cart/settings.py
@@ -123,5 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SECURE_REFERRER_POLICY = 'no-referrer'
